9805e184ae582c2b749d7038071c2b388d858742
mariia-yakovenko <marie.iakovenko@gmail.com>
2026-01-21 13:08:55 +0000

    Style changes and API

--- src/services/api.js
+++ src/services/api.js
@@ -1,30 +1,36 @@
-import axios from 'axios';
+import axios from "axios";
 
-const BASE_URL = 'https://api.sheety.co/a9a3922f064f47011cd1091c07d29c94/blog';
+const BASE_URL = "https://api.sheety.co/d4f01202612fa0dc1c10f95812deb78c/blog";
 
 const api = axios.create({
   baseURL: BASE_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
-export const getPosts = () => api.get('/posts').then(res => res.data.posts || []);
-export const createPost = (post) => api.post('/posts', { post }).then(res => res.data.post);
+export const getPosts = () =>
+  api.get("/posts").then((res) => res.data.posts || []);
+export const createPost = (post) =>
+  api.post("/posts", { post }).then((res) => res.data.post);
 export const updatePost = (id, post) => api.put(`/posts/${id}`, { post });
 export const deletePost = (id) => api.delete(`/posts/${id}`);
 
-export const getComments = () => api.get('/comments').then(res => res.data.comments || []);
-export const createComment = (comment) => api.post('/comments', { comment }).then(res => res.data.comment);
-export const updateComment = (id, comment) => api.put(`/comments/${id}`, { comment });
+export const getComments = () =>
+  api.get("/comments").then((res) => res.data.comments || []);
+export const createComment = (comment) =>
+  api.post("/comments", { comment }).then((res) => res.data.comment);
+export const updateComment = (id, comment) =>
+  api.put(`/comments/${id}`, { comment });
 
 export const getCommentsByPostId = async (post_id) => {
   const comments = await getComments();
   return comments
-    .filter(c => c.postId === Number(post_id) && c.isApproved)
+    .filter((c) => c.postId === Number(post_id) && c.isApproved)
     .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
 };
 
 export const getCommentsCountByPostId = async (post_id) => {
   const comments = await getComments();
-  return comments.filter(c => c.postId === Number(post_id) && c.isApproved).length;
+  return comments.filter((c) => c.postId === Number(post_id) && c.isApproved)
+    .length;
 };
